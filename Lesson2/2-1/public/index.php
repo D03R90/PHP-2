b5d799bb9da0a4f49e840bd67344d040a9759fc8
D03R90 <vadim.tereschenko@mail.ru>
2018-12-25 19:58:40 +0300

    updated "Index"

--- Lesson2/2-1/public/index.php
+++ Lesson2/2-1/public/index.php
@@ -1,19 +1,24 @@
 <?php
+
+use app\engine\Autoload;
+use app\model\Products;
+use app\engine\Db;
+
 include "../engine/Autoload.php";
 
 spl_autoload_register([new \app\engine\Autoload(), 'loadClass']);
 
-// $product = new Products(new Db());
+$product = new Products(new Db());
 
-$product = new \app\model\Products("1", "Продукт", "Описание", "150");
+// $product = new app\model\Products("1", "Продукт", "Описание", "150");
 var_dump($product);
 
-echo"<hr>";
-$order = new \app\model\Orders("2", "Продукт", "Описание", "150", "150");
-var_dump($order);
+// echo"<hr>";
+// $order = new app\model\Orders("2", "Продукт", "Описание", "150", "150");
+// var_dump($order);
 
-echo"<hr>";
-$cart = new \app\model\Cart("3", "Продукт", "Описание", "150", "150");
-var_dump($cart);
+// echo"<hr>";
+// $cart = new app\model\Cart("3", "Продукт", "Описание", "150", "150");
+// var_dump($cart);
 
 //Тут я не стал делать проект рабочим, в папке 2-2 работающая версия (просто реализован логику пространства имён).
